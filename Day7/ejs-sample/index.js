0abecfb32f5c31a6badca9638811671a63bd342b
Arul Frances <arulfrances@gmail.com>
2024-04-03 21:17:36 +0530

    Day 9 Content Added

--- Day7/ejs-sample/index.js
+++ Day7/ejs-sample/index.js
@@ -11,6 +11,8 @@ app.get("/", function(req,res){
     res.render("welcome",{name: 'Arul',country: 'India', state:'Tamil Nadu', city:'Coimbatore',Nationality:'Indian'});
 });
 
+app.get("/about")
+
 app.listen(5000, function(req,res){
     console.log("Server Connected to port 5000");
 });
---
+++ Day9/second-react-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <> <div className="App">
+      <header className="App-header">
+        <div>Welcome to MERN Stack Session</div>
+        <div><b>We are currently building our second project in React</b></div>
+        <div>End of Session</div>
+      </header>
+      
+    </div>
+    <div>Footer</div>
+    </>
+  );
+}
+
+export default App;

259e64cec31fd0992d68672c37fc6c61fbfd0ec7
Arul Frances <arulfrances@gmail.com>
2024-04-09 21:07:30 +0530

    Day 14 Content Added

--- Day12-13-14/weatherdashboard/src/Dashboard.js
+++ Day12-13-14/weatherdashboard/src/Dashboard.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import './Dashboard.css';
 import axios from 'axios';
-// import daycloud from './assets/01d.png';
-// import nightcloud from './assets/01n.png';
+import daycloud from './assets/01d.png';
+import nightcloud from './assets/01n.png';
+import images from "./images";
 
 function Dashboard() {
     const [data, setData] = useState({});
@@ -62,17 +63,24 @@ function Dashboard() {
         <div className="weatherSummary">
             <div className="temperatureValue">
 
-       <img src="assets/01d.png"/>        
-       <img src={data.main ? `assets/${data.weather.icon}.png` : null}/>
+      
 
-        {/* {data.main ?  <img src={daycloud} height="60px" width="60px"></img>: "" }<br/> */}
+         {data.main ?  <img src={daycloud} height="60px" width="60px"></img> : "" }<br/> 
          <b>Temperature:</b> {data.main ? data.main.temp.toFixed(1) : null} <br/>
          <b>Feels Like:</b> {data.main ? data.main.feels_like.toFixed(1) : null} <br/>
          <b>Min. Temperature:</b> {data.main ? data.main.temp_min.toFixed(1) : null} <br/>
          <b>Max. Temperature:</b> {data.main ? data.main.temp_max.toFixed(1) : null} <br/>
          <b>Pressure:</b> {data.main ? data.main.pressure : null} <br/>
          <b>Humidity:</b> {data.main ? data.main.humidity : null} <br/>
+
+         <img src={require ('./assets/01d.png')} height="60px" width="60px"></img>
+
+         <img src={ images.nightlogo } height="40px" width="80px"></img>
+
+         {data.main ? data.weather[0].icon : null }
          
+         {data.main ? <img src={`assets/{data.weather[0].icon}.png`}></img> : ""}
+
             </div>
 
             <div className="weatherDescription">
---
+++ Day12-13-14/weatherdashboard/src/images.js
@@ -0,0 +1,6 @@
+const images = {
+    daylogo: require('./assets/01d.png').default,
+    nightlogo: require('./assets/01n.png').default,
+}
+
+export default images;
